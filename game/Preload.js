dcb11fcb4724d3a3954a36cd302f031bcf43ecd1
limon <gendalf44@yandex.ru>
2012-12-16 01:06:10 +0400

    fix 2nd preloader on complete

--- game/Preload.js
+++ game/Preload.js
@@ -13,15 +13,17 @@ Preloader = function(){
 	this.pr.onProgress = this.onProgress;
 	this.onComplete = null;
 	this.pr.onComplete = function(event){
-		$("#preloader").fadeOut();
-		if(event.target.owner.onComplete) event.target.owner.onComplete();		
+		if(event.target.owner.onComplete){
+			event.target.owner.onComplete();
+			$("#preloader").fadeOut();
+		}
 	};
 	
 	this.onProgress = null;
 	
 	//this.pr.onFileProgress = handleFileProgress;
 	//this.pr.onError = handleFileError;
-	this.pr.setMaxConnections(1);		
+	this.pr.setMaxConnections(5);		
 };
 
 
@@ -38,6 +40,7 @@ Preloader.prototype.onFileLoad = 	function(event){
 		if (n>=0){
 			event.target.owner.imgs[event.target.owner.names[n]] = event.result;
 		}		
+		console.log(event.target.owner.names[n]);
 };
 
 
@@ -75,5 +78,26 @@ Preloader.prototype.initConstants = function()
    this.addImage("img/image2.jpg","name2");
    this.addImage("img/spider.png","spider");
    this.addImage("img/back.jpg","back");
+   this.addImage("img/back/pattern.png","BackGround");
+   this.addImage("img/back/cloud_1.png","cloud_1");
+   this.addImage("img/back/cloud_2.png","cloud_2");
+   this.addImage("img/back/cloud_3.png","cloud_3");
+   this.addImage("img/back/big_1.png","big_1");
+   this.addImage("img/back/big_2.png","big_2");
+   this.addImage("img/back/big_3.png","big_3");
+   this.addImage("img/back/big_4.png","big_4");
+   this.addImage("img/back/big_5.png","big_5");
+   this.addImage("img/back/big_6.png","big_6");
+   this.addImage("img/back/big_7.png","big_7");
+   this.addImage("img/back/lit_mount_1.png","lit_mount_1");
+   this.addImage("img/back/lit_mount_2.png","lit_mount_2");
+   this.addImage("img/back/lit_mount_3.png","lit_mount_3");
+   this.addImage("img/back/mount_1.png","mount_1");
+   this.addImage("img/back/mount_2.png","mount_2");
+   this.addImage("img/back/small_1.png","small_1");
+   this.addImage("img/back/small_2.png","small_2");
+   this.addImage("img/back/small_3.png","small_3");
+   this.addImage("img/back/small_4.png","small_4");
+   this.addImage("img/back/small_5.png","small_5");
 };
 
--- game/view/BackGround.js
+++ game/view/BackGround.js
@@ -1,3 +1,5 @@
 function BackGround(){
-	
+	//this.
 }
+
+extend(BackGround,createjs.Container);
