0ffbb6a4f124d1ba9456caaaededc66820180747
limon <gendalf44@yandex.ru>
2012-12-22 21:34:15 +0400

    +record table

--- game/scenes/RecordsScene.js
+++ game/scenes/RecordsScene.js
@@ -1,9 +1,18 @@
 function RecordsScene(){
 	$("#backButton_records").bind('click',this.onBackCkick);
+	
+	 jQuery.ajax({
+            type: "GET", // метод передачи данных, можно пропустить - по умолчанию и так get
+            url: "http://hahaton.ru/html5games/reflect/table/index.php", // путь к файлу, который будем читать
+            dataType: "xml", // тип данных, с которыми работаем
+            success: this.onRecordsCome,
+            data: {mode: 'showall'}
+            });
+		
 }
 extend(RecordsScene,BaseScene);
 
-RecordsScene.prototype.show = function(){	
+RecordsScene.prototype.show = function(){
 	$("#records").fadeIn();
 }
 
@@ -14,4 +23,14 @@ RecordsScene.prototype.hide = function(){
 
 RecordsScene.prototype.onBackCkick = function(){
 	global.sceneController.switchScene(SceneController.eventTypes.MAIN_MENU);
+}
+
+RecordsScene.prototype.onRecordsCome = function(xml){
+	$('#recordTable').html('');
+	jQuery(xml).find('name').each(function(){
+		var name = jQuery(this).attr('name');
+		var count = jQuery(this).attr('count');
+		$('#recordTable').append('<tr><td>' + name + '</td><td>' + count + '</td></tr>');
+		console.log(name, count);
+	});
 }
