056c19842ccfe69851d01b630e225fb1cbfb66cf
limon <gendalf44@yandex.ru>
2013-01-03 04:12:43 +0400

    +info +credits +settings +top10 +cool back button

--- game/jq_ui.js
+++ game/jq_ui.js
@@ -14,6 +14,36 @@ $(function() {
 	
 	$('#info_center_content').load('scenes/html/info.html');
 	
-	$('#credits_center_content').load('scenes/html/credits.html');	
+	$('#credits_center_content').load('scenes/html/credits.html');
+	
+	$('.backButton').append(
+		'<div class="backButton_img1" style="float:left; width:63px; height:63px; background-image:url(\'img/menu/back_arrow/1_standart.png\');  position:absolute; top:0px; left:0px;"></div>'
+			+		
+		'<div class="backButton_img1_hover" style="display:none; width:63px; height:63px; background-image:url(\'img/menu/back_arrow/1_hover.png\');  position:absolute; top:0px; left:0px;"></div>'
+			+
+		'<div class="backButton_img2" style="float:left; width:63px; height:63px; background-image:url(\'img/menu/back_arrow/2_standart.png\');  position:absolute; top:0px; left:45px;"></div>'
+			+
+		'<div class="backButton_img2_hover" style="display:none; width:63px; height:63px; background-image:url(\'img/menu/back_arrow/2_hover.png\');  position:absolute; top:0px; left:45px;"></div>'
+	);
+	
+	var speed_anim = 300;
+	$('.backButton').hover(function(){
+		$(this).find('.backButton_img1_hover').css('display', 'block');
+		$(this).find('.backButton_img2_hover').css('display', 'block');
+		
+		$(this).find('.backButton_img2_hover').stop();
+		$(this).find('.backButton_img2').stop();
+		$(this).find('.backButton_img2_hover').animate({left : "30px"}, speed_anim);
+		$(this).find('.backButton_img2').animate({left : "30px"}, speed_anim);
+	},function(){
+		$(this).find('.backButton_img1_hover').css('display', 'none');
+		$(this).find('.backButton_img2_hover').css('display', 'none');
+		
+		$(this).find('.backButton_img2_hover').stop();
+		$(this).find('.backButton_img2').stop();
+		$(this).find('.backButton_img2_hover').animate({left : "45px"}, speed_anim);
+		$(this).find('.backButton_img2').animate({left : "45px"}, speed_anim);
+		
+	});
 	
 });
